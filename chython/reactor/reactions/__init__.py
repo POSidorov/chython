c15b2986c9b4a2d699baad7b0d983a640fc9d0b2
Bala Sakthivel <bala@icredd.hokudai.ac.jp>
2025-11-10 16:55:32 +0900

    init py updated

--- chython/reactor/reactions/__init__.py
+++ chython/reactor/reactions/__init__.py
@@ -42,6 +42,11 @@ from ._gilman import template as gilman_template
 from ._heck import template as heck_template
 from ._horner_wadsworth_emmons import template as horner_wadsworth_emmons_template
 from ._nazarov_cyclization import template as nazarov_cyclization_template
+from ._kabachnik_fields import template as kabachnik_fields_template
+from ._mannich import template as mannich_template
+from ._passerini import template as passerini_template
+from ._pauson_khand import template as pauson_khand_template
+from ._robinson_annulation import as robinson_annulation_template
 from ..reactor import Reactor, fix_mapping_overlap
 from ... import smarts, ReactionContainer, MoleculeContainer
 
